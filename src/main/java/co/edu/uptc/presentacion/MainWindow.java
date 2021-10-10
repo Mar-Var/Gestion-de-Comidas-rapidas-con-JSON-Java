f7a9c44bd63b54d5dbc67b41f3044a7957140331
Marcos-Vargas <marcos.vargas@uptc.edu.co>
2021-10-10 10:14:46 -0500

    GUI terminada

--- src/main/java/co/edu/uptc/presentacion/MainWindow.java
+++ src/main/java/co/edu/uptc/presentacion/MainWindow.java
@@ -7,10 +7,12 @@ import java.awt.Insets;
 import java.awt.Scrollbar;
 import java.awt.event.KeyEvent;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
@@ -62,7 +64,7 @@ public class MainWindow extends JFrame {
 	private JScrollPane spProductos;
 	private JButton btnAgregarproducto;
 	private JButton btnActualizarProducto;
-	private JButton btnBuscarProducto;
+
 	private JButton btnEliminarProducto;
 	
 	private JPanel panelpedidos;
@@ -71,13 +73,19 @@ public class MainWindow extends JFrame {
 	private JLabel lbCedulaCliente;
 	private JTextField txtCedulaCliente;
 	private JLabel lbNombreCiente;
+	private JTextField txtNombreCliente;
 	private JLabel lbDireccionCliente;
+	private JTextField txtDireccioncliente;
 	private JLabel lbTelefonoCliente;
+	private JTextField txtTelefonoCliente;
 	private JLabel lbDomiciliario;
 	private JComboBox cbDomiciliario;
 	private JComboBox cbProductos;
 	private JLabel lbCantidadProductos;
 	private JTextField txtCantidadProductos;
+	private JList ltProductosPedido = new  JList();
+	private DefaultListModel dfltProductospedido = new  DefaultListModel();
+	private JScrollPane jsProductosPedido = new JScrollPane();
 	private JButton btnAgregarCarrito;
 	private JButton btnTerminarpedido;
 	private JButton btnLimpiarPedidos;
@@ -92,6 +100,7 @@ public class MainWindow extends JFrame {
 	private JTable tblventasporFecha;
 	private DefaultTableModel dftblVentasPorFecha;
 	private JScrollPane scVentasPorfecha;
+	private JButton btnBuscarProducto;
 	
 	private JPanel panelInferiorIgestionesExtras;
 	private JButton btnObtenerLiquidacion;
@@ -111,7 +120,7 @@ public class MainWindow extends JFrame {
 		super("Rapidos y Furiosos");
 		setExtendedState(MAXIMIZED_BOTH);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);// Decir que pasa cuando se cierre la ventana
-		setMinimumSize(new Dimension(800,700));
+		setMinimumSize(new Dimension(1000,700));
 		setResizable(true);
 		
 	}
@@ -132,18 +141,18 @@ public class MainWindow extends JFrame {
 		panelInternoDomiciliario.setBorder(new TitledBorder("Informacion del domiciliario"));
 		panelInternoDomiciliario.setLayout(new GridBagLayout());
 		
-		lbnombreDomiciliario=new JLabel("Nombre");
-		txtnombreDomiciliario=new JTextField();
+		lbnombreDomiciliario=new JLabel("Identificacion del Domiciliario");
+		txtnombreDomiciliario=new JTextField(20);
 		lbApellidosDomiciliario=new JLabel("Apellidos");
-		txtApellidosDomiciliario= new JTextField();
+		txtApellidosDomiciliario= new JTextField(20);
 		lbCedula=new JLabel("Cedula");
-		txtCedula=new JTextField();
+		txtCedula=new JTextField(20);
 		lbfechaNacimiento=new JLabel("Fecha nacimiento");
 		dcFechaNacimiento= new JDateChooser();
 		lbDireccion= new JLabel("Direccion");
-		txtDireccion=new JTextField();
+		txtDireccion=new JTextField(20);
 		lbTelefono= new JLabel("Telefono");
-		txtTelefono= new JTextField();
+		txtTelefono= new JTextField(20);
 		btnAgregar=new JButton("Agregar");
 		btnActualizar=new JButton("Actualizar");
 		btnEliminar=new JButton("Eliminar");
@@ -159,9 +168,9 @@ public class MainWindow extends JFrame {
 		panelInternoProductos.setBorder(new TitledBorder("Informacion de productos"));
 		
 		lbNombreProducto= new JLabel("Nombre");
-		txtNombreProducto= new JTextField();
+		txtNombreProducto= new JTextField(20);
 		lbPrecioProducto= new JLabel("Precio");
-		txtPrecioProducto= new JTextField(); 
+		txtPrecioProducto= new JTextField(20); 
 		dftblProductos= new DefaultTableModel(new Object[] {"Nombre","Costo"},0);
 		tblProductos= new JTable(dftblProductos);
 		spProductos= new JScrollPane(tblProductos);
@@ -175,18 +184,22 @@ public class MainWindow extends JFrame {
 		
 		panelInternoPedidos=new JPanel();
 		panelInternoPedidos.setLayout(new GridBagLayout());
-		panelInternoPedidos.setBorder(new TitledBorder("Pedidos"));
+		panelInternoPedidos.setBorder(new TitledBorder("Datos del Pedido"));
 		
 		lbCedulaCliente=new JLabel("Cedula Cliente");
-		txtCedulaCliente=new JTextField();
+		txtCedulaCliente=new JTextField(20);
 		lbNombreCiente=new JLabel("Nombre Cliente");
 		lbDireccionCliente=new JLabel("Direccion");
 		lbTelefonoCliente=new JLabel("Telefono");
 		lbDomiciliario=new JLabel("Domiciliario");
-		cbDomiciliario= new JComboBox<>();
+		cbDomiciliario= new JComboBox();
 		cbProductos=new JComboBox<>();
 		lbCantidadProductos=new JLabel("Cantidad");
-		txtCantidadProductos= new JTextField();
+		txtCantidadProductos= new JTextField(10);
+		
+		dfltProductospedido = new  DefaultListModel();
+		ltProductosPedido = new  JList(dfltProductospedido);
+		jsProductosPedido = new JScrollPane(ltProductosPedido);
 		btnAgregarCarrito=new JButton("Agregar al carrito");
 		btnTerminarpedido= new JButton("Terminar pedido");
 		btnLimpiarPedidos= new JButton("Limpiar campos");
@@ -202,7 +215,7 @@ public class MainWindow extends JFrame {
 		lbFechaVenta= new JLabel("Fecha de venta");
 		lbTotalVentas = new JLabel("Total de venta");
 		dcFechaVentas= new JDateChooser();
-		txtTotalventas= new JTextField();
+		txtTotalventas= new JTextField(20);
 		txtTotalventas.setEnabled(false);
 		dftblVentasPorFecha = new DefaultTableModel(new Object[] {"Nombre Cliente","Apellido Cliente"
 				,"Celular","Direccion","Identificacion Domiciliario","Productos Ordenados","Fecha","Costo"},0);
@@ -223,10 +236,15 @@ public class MainWindow extends JFrame {
 		panelInferiorDgestionesExtras.setLayout(new GridBagLayout());
 		panelInferiorDgestionesExtras.setBorder(new TitledBorder("Producto con mayor preferencia"));
 		panelInferiorDgestionesExtras.setSize(400, 400);
-		txtProductoPreferente= new JTextField();
+		txtProductoPreferente= new JTextField(20);
+		txtProductoPreferente.setEnabled(false);
 		dtblPreferencias= new DefaultTableModel(new Object[] {"Nombre","Cantidad"},0);
 		tblPreferencias= new JTable(dtblPreferencias);
 		spPreferencias= new JScrollPane(tblPreferencias);
+		txtNombreCliente= new JTextField(20);
+		txtDireccioncliente= new JTextField(20);
+		txtTelefonoCliente = new JTextField(20);
+		btnObtenerPreferencia=new JButton("Obtener");
 		
 		
 	}
@@ -238,9 +256,9 @@ public class MainWindow extends JFrame {
 		tabbedPane.add("Domiciliario",panelDomiciliario);
 		addComponentToProducts();
 		tabbedPane.add("Productos",panelProductos);
-		
+		addComponentsToPanelPedidos();
 		tabbedPane.add("Pedidos",panelpedidos);
-		
+		addComponentGestiones();
 		tabbedPane.add("Procesos",panelGestionesExtras);
 
 
@@ -250,7 +268,7 @@ public class MainWindow extends JFrame {
 	
 	public void addcomponentsTopanelDomiciliario() {
 		GridBagConstraints g = new GridBagConstraints();
-		g.weightx=1.0;
+		g.insets= new Insets(10, 10, 10, 10);
 		g.gridx=0;
 		g.gridy=0;
 		g.fill=GridBagConstraints.HORIZONTAL;
@@ -263,11 +281,11 @@ public class MainWindow extends JFrame {
 	public void addcomponentsTopanelInternalDomiciliario() {
 		GridBagConstraints g = new GridBagConstraints();
 
-		g.anchor=GridBagConstraints.CENTER;
-		g.weightx=1.0;
+
+		g.weightx=0.1;
 		g.gridx=0;
 		g.gridy=0;
-		
+		g.fill=GridBagConstraints.BOTH;
 		g.insets= new Insets(10, 0, 10, 20);
 		panelInternoDomiciliario.add(lbnombreDomiciliario,g);
 		g.gridy=1;
@@ -276,7 +294,7 @@ public class MainWindow extends JFrame {
 		panelInternoDomiciliario.add(lbCedula,g);
 		g.gridy=0;
 		g.gridx=1;
-		g.fill=GridBagConstraints.BOTH;
+
 		panelInternoDomiciliario.add(txtnombreDomiciliario,g);
 		g.gridy=1;
 		panelInternoDomiciliario.add(txtApellidosDomiciliario,g);
@@ -311,19 +329,18 @@ public class MainWindow extends JFrame {
 	
 	public void addComponentToProducts() {
 		GridBagConstraints g = new GridBagConstraints();
-
-		g.weightx=1.0;
+		g.insets= new Insets(10, 10, 10, 10);
+	//	g.weightx=1.0;
 		g.gridx=0;
 		g.gridy=0;
 		panelProductos.add(new JLabel("Gestion de productos"),g);
-		g.fill=GridBagConstraints.HORIZONTAL;
+		//g.fill=GridBagConstraints.HORIZONTAL;
 		g.anchor=GridBagConstraints.CENTER;
 		g.gridy=1;
 		addComponentToProductsInternalPanel();
 		panelProductos.add(panelInternoProductos,g);
 		g.gridy=2;
-		g.weightx=0.0;
-		g.fill=GridBagConstraints.NONE;
+	//	g.fill=GridBagConstraints.NONE;
 		panelProductos.add(spProductos,g);
 	}
 	public void addComponentToProductsInternalPanel() {
@@ -332,9 +349,8 @@ public class MainWindow extends JFrame {
 		g.weightx=1.0;
 		g.gridx=0;
 		g.gridy=0;
-		g.insets=new Insets(10, 20, 10, 20);
 		g.anchor=GridBagConstraints.CENTER;
-		
+		g.insets= new Insets(10, 10, 10, 10);
 		panelInternoProductos.add(lbNombreProducto,g);
 		g.gridy=1;
 		g.fill=GridBagConstraints.BOTH;
@@ -344,14 +360,14 @@ public class MainWindow extends JFrame {
 
 		panelInternoProductos.add(txtNombreProducto,g);
 		g.gridy=1;
-		//g.fill=GridBagConstraints.NONE;
+		g.fill=GridBagConstraints.NONE;
 		panelInternoProductos.add(btnActualizarProducto,g);
 		g.gridx=3;
 		g.gridy=0;
-		g.fill=GridBagConstraints.NONE;
 		panelInternoProductos.add(lbPrecioProducto,g);
 		g.gridy=1;
 		g.fill=GridBagConstraints.BOTH;
+		g.weightx=0.0;
 		panelInternoProductos.add(btnEliminarProducto,g);
 		g.gridx=4;
 		g.gridy=0;
@@ -362,7 +378,168 @@ public class MainWindow extends JFrame {
 	
 	public void addComponentsToPanelPedidos() {
 		GridBagConstraints g = new GridBagConstraints();
+		g.insets=new Insets(10, 10, 10, 10);
+		g.gridx=0;
+		g.gridy=0;
+		g.gridwidth=2;
+		g.fill=GridBagConstraints.HORIZONTAL;
+		addComponentsToPanelPedidosInternalPlanel();
+		panelpedidos.add(panelInternoPedidos,g);
+		g.insets=new Insets(0, 0, 0, 0);
+		g.gridwidth=1;
+		g.gridy=1;
+		g.fill=GridBagConstraints.NONE;
+	//	g.anchor=GridBagConstraints.CENTER;
+	//	panelpedidos.add(btnTerminarpedido,g);
+	//	g.gridx=1;
+	//	panelpedidos.add(btnLimpiarPedidos,g);
+		
 	}
+	public void addComponentsToPanelPedidosInternalPlanel() {
+		
+		GridBagConstraints g = new GridBagConstraints();
+		g.insets= new Insets(10, 10, 10, 10);
+		g.fill= GridBagConstraints.HORIZONTAL;
+		g.weightx=1.0;
+		g.anchor=GridBagConstraints.WEST;
+		g.gridx=0;
+		g.gridy=0;
+		panelInternoPedidos.add(lbCedulaCliente,g);
+		g.gridx=1;
+
+		panelInternoPedidos.add(txtCedulaCliente,g);
+		//g.fill= GridBagConstraints.NONE;
+		g.gridx=2;
+		panelInternoPedidos.add(new JLabel("Identificacion del Domiciliario"),g);
+		g.gridx=3;
+		g.weightx=2;
+		//g.fill= GridBagConstraints.NONE;
+		panelInternoPedidos.add(cbDomiciliario,g);
+
+		g.weightx=1;
+		g.gridy=1;
+		g.gridx=0;
+		panelInternoPedidos.add(lbNombreCiente,g);
+		g.gridx=1;
+		//g.fill= GridBagConstraints.BOTH;
+		panelInternoPedidos.add(txtNombreCliente,g);
+		g.gridx=2;
+		//g.fill=GridBagConstraints.NONE;
+		panelInternoPedidos.add(cbProductos,g);
+		g.gridx=3;
+		panelInternoPedidos.add(lbCantidadProductos,g);
+		g.gridx=4;
+		//g.fill=GridBagConstraints.BOTH;
+		panelInternoPedidos.add(txtCantidadProductos,g);
+		//g.fill=GridBagConstraints.NONE;
+		g.gridy=2;
+		g.gridx=0;
+		//g.fill= GridBagConstraints.HORIZONTAL;
+		panelInternoPedidos.add(lbDireccionCliente,g);
+		g.gridx=1;
+		
+		panelInternoPedidos.add(txtDireccioncliente,g);
+		//g.fill= GridBagConstraints.NONE;
+		g.gridx=2;
+		g.weighty=2;
+		panelInternoPedidos.add(jsProductosPedido,g);
+		g.weighty=1;
+		g.gridx=3;
+		panelInternoPedidos.add(btnAgregarCarrito,g);
+		g.gridy=3;
+		g.gridx=3;
+		g.fill=GridBagConstraints.NONE;
+		g.anchor=GridBagConstraints.CENTER;
+		panelInternoPedidos.add(btnTerminarpedido,g);
+		g.gridx=4;
+		panelInternoPedidos.add(btnLimpiarPedidos,g);
+		
+	}
+	public void addComponentGestiones() {
+		GridBagConstraints g = new GridBagConstraints();
+		g.insets=new Insets(10, 10, 10, 10);
+		g.weightx=1.0;
+		g.weighty=1.0;
+		g.gridwidth=2;
+		g.gridx=0;
+		g.gridy=0;
+		g.fill=GridBagConstraints.BOTH;
+		addComponentGestionespanelSuperior();
+		panelGestionesExtras.add(panelSuperiorgestionesExtras,g);
+		g.gridwidth=1;
+		g.gridy=1;
+		g.gridx=0;
+		addComponentGestionespanelinferiorI();
+		panelGestionesExtras.add(panelInferiorIgestionesExtras,g);
+		g.gridx=1;
+		addComponentGestionespanelinferiorD();
+		panelGestionesExtras.add(panelInferiorDgestionesExtras,g);
+		
+	}
+	public void addComponentGestionespanelSuperior() {
+		GridBagConstraints g = new GridBagConstraints();
+		g.weightx=1.0;
+
+		g.gridx=0;
+		g.gridy=0;
+		g.fill=GridBagConstraints.BOTH;
+		g.insets=new Insets(10, 10, 10, 10);
+		panelSuperiorgestionesExtras.add(lbFechaVenta,g);
+		g.gridx=1;
+		panelSuperiorgestionesExtras.add(dcFechaVentas,g);
+		g.gridx=2;
+		panelSuperiorgestionesExtras.add(btnBuscarProducto,g);
+		g.gridx=3;
+		panelSuperiorgestionesExtras.add(lbTotalVentas,g);
+		g.gridx=4;
+		panelSuperiorgestionesExtras.add(txtTotalventas,g);
+		g.gridwidth=5;
+		g.gridx=0;
+		g.gridy=1;
+		g.weighty=1.0;
+		panelSuperiorgestionesExtras.add(scVentasPorfecha,g);
+		
+		
+		
+		
+	}
+	public void addComponentGestionespanelinferiorI() {
+		GridBagConstraints g = new GridBagConstraints();
+		g.insets=new Insets(10, 10, 10, 10);
+		g.gridx=1;
+		g.gridy=0;
+		g.weightx=1.0;
+
+		panelInferiorIgestionesExtras.add(btnObtenerLiquidacion,g);
+		g.fill=GridBagConstraints.BOTH;
+		g.weightx=0.0;
+		g.gridy=1;
+		g.gridwidth=2;
+		g.weighty=1.0;
+		panelInferiorIgestionesExtras.add(spLiquidaciones,g);
+		
+		
+		
+	}
+	public void addComponentGestionespanelinferiorD() {
+		GridBagConstraints g = new GridBagConstraints();
+		g.insets=new Insets(10, 10, 10, 10);
+		g.gridx=0;
+		g.gridy=0;
+		g.weightx=1.0;
+		panelInferiorDgestionesExtras.add(btnObtenerPreferencia,g);
+		g.gridx=1;
+		g.fill= GridBagConstraints.HORIZONTAL;
+		panelInferiorDgestionesExtras.add(txtProductoPreferente,g);
+		g.fill=GridBagConstraints.BOTH;
+		g.weightx=0.0;
+		g.gridy=1;
+		g.gridx=0;
+		g.gridwidth=2;
+		g.weighty=1.0;
+		panelInferiorDgestionesExtras.add(spPreferencias,g);
+	}
+	
 	
 	
 }
